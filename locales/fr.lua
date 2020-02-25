77298b040555b1917ce3c364ae41ff0a89942b71
ROTY Jérémy <rotyjeremy@gmail.com>
2020-02-25 11:53:11 +0100

    adding french version ; change html interface

---
+++ locales/fr.lua
@@ -0,0 +1,11 @@
+Locales['fr'] = {
+	['show_registration'] = 'Voir le menu d\'enregistrement',
+	['show_active_character'] = 'show Active Character',
+	['delete_character'] = 'delete Your Character And Create A New One',
+	['deleted_character'] = 'Votre personnage a été supprimé.',
+	['not_registered'] = 'Vous n\'avez pas de personnage enregistré.',
+	['active_character'] = '~b~Personnage activé:~s~ %s %s',
+	['already_registered'] = 'Vous avez déjà un personnage',
+	['failed_identity'] = 'Impossible de modifier votre personnage ; demandez à un admin',
+	['create_a_character'] = 'Vous venez de créer votre personnage.'
+}
